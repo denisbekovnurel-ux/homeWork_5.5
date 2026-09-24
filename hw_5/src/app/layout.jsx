18b0b9ce66539e0085f27b6373b0a0cae53f1e9c
Nurel <denisbekovnurel@gmail.com>
2026-09-24 17:29:43 +0600

    hw5.5: улучшил layout

--- hw_5/src/app/layout.jsx
+++ hw_5/src/app/layout.jsx
@@ -3,7 +3,7 @@ import { Header } from '../components/header.jsx'
 
 export function Layout() {
     return (
-        <div className='max-w-[1200px] mx-auto'>
+        <div className='max-w-[1200px] mx-auto px-4'>
             <Header />
             <main>
                 <Outlet />
